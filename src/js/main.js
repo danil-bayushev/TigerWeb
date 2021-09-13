b0e90da88725632f1aaf3493f54bc15a6ae49c46
Danil Bayushev <danil.bayushev@gmail.com>
2021-09-13 17:32:54 +0300

    Js

--- src/js/main.js
+++ src/js/main.js
@@ -10,22 +10,40 @@ const $ = {
 window.onload = function() {
     let catalog_opened = false;
     const background = $.gE('.background');
-    const catalogBtn = $.gE('.catalog');
+    const catalogBtns = $.gE_s('.catalog');
     const openCatalogBtn = $.gE('.catalog_open .catalog');
+    const catalogSections = $.gE_s('.catalog_open__wrapper .section');
     const productsTumbs = $.gE_s('.tumbs>ul>li');
     const productsMainImg = $.gE('.main_image>img');
+    const fixedMenu = $.gE('.fixed_menu');
+    let vertical_Menu_opened = false;
+    const verticalMenu = $.gE('.vertical_menu');
+    const kebabMenuBtn = $.gE_s('.kebab_menu');
+    const tabs = $.gE_s('.tab');
+
+
     
-
-    catalogBtn.addEventListener('click', openCatalog);
-    openCatalogBtn.addEventListener('click', closeCatalog);
-    background.addEventListener('click', closeCatalog);
-
     changeCirclesBg();
 
+   console.log(catalogSections);
+
+    catalogBtns.forEach((el) => {
+        el.addEventListener('click', openCatalog);
+    });
+    openCatalogBtn.addEventListener('click', closeAll);
+    background.addEventListener('click', closeAll);
     productsTumbs.forEach((el) => {
         el.addEventListener('click', changeImg);
-    })
+    });
+    window.addEventListener('scroll', showFixedMenu);
+
+    kebabMenuBtn.forEach((el) => {
+        el.addEventListener('click', showVerticalMenu);
+    });
     
+    tabs.forEach((el) => {
+        el.addEventListener('click', selectTab);
+    })
 
 
 
@@ -36,14 +54,20 @@ window.onload = function() {
         if(!catalog_opened){
             $.gE('.catalog_open').style.display = 'block';
             background.style.display = 'block';
+
             catalog_opened = true;
         }
     }
-    function closeCatalog(){
+    function closeAll(){
         if(catalog_opened){
             $.gE('.catalog_open').style.display = 'none';
             background.style.display = 'none';
             catalog_opened = false;
+        }
+        if(vertical_Menu_opened){
+            verticalMenu.style.display = 'none';
+            background.style.display = 'none';
+            vertical_Menu_opened = false;
         }
     }
     function changeCirclesBg(){
@@ -71,6 +95,35 @@ window.onload = function() {
             el.classList.remove('active');
         })
         tumb.classList.add('active');
+    }
+
+    function showFixedMenu(){
+        console.log(window.scrollY);
+        if(window.scrollY > 38){
+            fixedMenu.style.display = 'block';
+        }else if(window.scrollY < 38){
+            fixedMenu.style.display = 'none';
+        }
+    }
+    function showVerticalMenu(){
+        if(!vertical_Menu_opened){
+            verticalMenu.style.display = 'block';
+            background.style.display = 'block';
+            vertical_Menu_opened = true;
+        }else if(vertical_Menu_opened){
+            verticalMenu.style.display = 'none';
+            background.style.display = 'none';
+            vertical_Menu_opened = false;
+        }
+    }
+    function selectTab(e){
+        if(e.target.classList.contains('active')) return;
+        tabs.forEach(el => {
+            if(el.classList.contains('active')){
+               el.classList.remove('active'); 
+            }
+        });
+        e.target.classList.add('active');
     }
 
 };
